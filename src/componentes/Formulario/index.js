70d3c1553148a89fbeacf1c89c0858032464ce39
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-08-11 17:00:44 -0300

    salvando colaboradores em lista usando hook

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
 import { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -23,7 +23,12 @@ const Formulario = () => {
     
     const aoSalvar = (evento) =>{
         evento.preventDefault()
-        console.log('form foi submetido',nome, cargo, imagem)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
