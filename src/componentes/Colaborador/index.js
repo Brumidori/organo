0a0a7a815e7ce78b962676e667ba5a9a4ecc78b5
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-08-11 17:56:09 -0300

    filtrando colaboradores por times

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({nome, imagem, cargo}) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/brumidori.png' alt='Bruna Midori' />
+                <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
-                <h4>Bruna Midori</h4>
-                <h5>Dev full-stack</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,12 +1,14 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time= (props) =>{
+const Time = (props) => {
     return (
         //style eh insersão de css no JSX
-        <section className='time' style={{backgroundColor: props.corSecundaria}}> 
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <Colaborador/>
+        <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
     )
 }
